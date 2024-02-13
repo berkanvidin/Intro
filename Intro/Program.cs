1de99072a7a4acbf4ab9d258927de10b3566d88f
Mehmet Berkan <mehmetberkanvdn@outlook.com>
2024-02-13 17:40:37 +0300

    İntro added

---
+++ Intro/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message = "Krediler";
+int term = 12;
+double amount = 100000.5;
+
+//variables --> camelCase
+bool isAuthenticated = true;
+Console.WriteLine(message);
+
+//condition 
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton-->Hoşgeldin Engin");
+}
+else
+{
+    Console.WriteLine("Buton-->Sisteme Giriş Yap");
+}
+
+
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" };
+//string[] loans2 = new string[6];
+//loans2[0] = "Kredi";
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+CourseManager courseManager = new ();
+Course[] courses2 = courseManager.getAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+
+Console.WriteLine("kod bitti");

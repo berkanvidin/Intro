06ca94662c2eb68e2d606189a4a485d08057b310
Mehmet Berkan <mehmetberkanvdn@outlook.com>
2024-02-19 14:16:47 +0300

    Day 2 Lesson 2 Commit 1

--- Intro/Business/CourseManager.cs
+++ Intro/Business/CourseManager.cs
@@ -1,4 +1,5 @@
-﻿using Intro.Entities;
+﻿using Intro.DataAccess.Concretes;
+using Intro.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,36 +10,12 @@ namespace Intro.Business;
 
 public class CourseManager
 {
-    Course[] courses=new Course[3];
-    //Constructor ( Yapıcı Blok )
-    public CourseManager() // CourseManager Newlendiği yani tanıtıldığı an çalışır.
+    
+    public List<Course> getAll()
     {
-        Course course1 = new Course();
-        course1.Id = 1;
-        course1.Name = "C#";
-        course1.Description = ".NET 8 vs...";
-        course1.Price = 0;
+        // business rules
+        CourseDal courseDal = new CourseDal();
 
-        Course course2 = new Course();
-        course2.Id = 1;
-        course2.Name = "Java";
-        course2.Description = "Java 17";
-        course2.Price = 10;
-
-        Course course3 = new Course();
-        course3.Id = 1;
-        course3.Name = "Python";
-        course3.Description = "Python 3.12...";
-        course3.Price = 20;
-
-        courses[0] = course1;
-        courses[1] = course2;
-        courses[2] = course3;
-
-        Console.WriteLine("Başladı...");
-    }
-    public Course[] getAll()
-    {
-        return courses;
+        return courseDal.GetAll();
     }
 }
